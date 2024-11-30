42b7712d9bb139068c36d777f0a260f11c570e22
MoussabAlhussien2614 <moussab2614@gmail.com>
2024-11-30 19:12:06 +0400

    update MemberOrAdmin middleware

--- app/Http/Middleware/MemberGroupMiddleware.php
+++ app/Http/Middleware/MemberGroupMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\Group;
 
 class MemberGroupMiddleware
 {
@@ -15,7 +16,8 @@ class MemberGroupMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-         $group = $request->route("group");
+         $group =Group::find( $request->route("groupId"));
+
          $user_id = auth()->user()->id;
 
          $userRole = $group->users()->where('user_id', $user_id)->first()->pivot->role;
